db8b958cfbe9e648887950e63952ec2b21d1a864
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-10-25 23:37:01 -0300

    user entity

--- entity/car.go
+++ entity/car.go
@@ -3,10 +3,11 @@ package entity
 import "github.com/google/uuid"
 
 type Car struct {
-	Id      uuid.UUID `json:"id"`
-	Model   string    `json:"model"`
-	Brand   string    `json:"brand"`
-	Hp      int       `json:"hp"`
-	License string    `json:"license"`
-	Price   int64     `json:"price"`
+	Id             uuid.UUID `json:"id"`
+	Model          string    `json:"model"`
+	Brand          string    `json:"brand"`
+	Hp             int       `json:"hp"`
+	License        string    `json:"license"`
+	InsurancePrice int64     `json:"insurance_price"`
+	OwnerId        string    `json:"owner_id"`
 }
