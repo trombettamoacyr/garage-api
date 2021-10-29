d6f906e5d8e656e9db5f3314ba5ebbfa0b79e1ae
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-10-29 01:10:11 -0300

    fix car-insurance service

---
+++ service/car-insurance.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"encoding/json"
+	"github.com/trombettamoacyr/garage-api/model"
+	"github.com/trombettamoacyr/garage-api/util"
+	"net/http"
+)
+
+type CarInsurance interface {
+	FetchValue() string
+}
+
+type carInsuranceService struct{}
+
+func NewCarInsuranceService() CarInsurance {
+	return &carInsuranceService{}
+}
+
+const (
+	carDetailsUrl = "https://myfakeapi.com/api/cars/"
+)
+
+func (*carInsuranceService) FetchValue() string {
+	randomNum := util.NewRandomNumber(1, 999)
+	urlApi := carDetailsUrl + randomNum
+
+	client := http.Client{}
+	resp, _ := client.Get(urlApi)
+
+	var insurance = model.Insurance{}
+	json.NewDecoder(resp.Body).Decode(&insurance)
+
+	return insurance.Car.InsuranceValue
+}
