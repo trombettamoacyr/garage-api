b873272d456773a7c703819944875bb6e51b2ad4
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-10-29 01:17:23 -0300

    fix car-details-service name

--- service/car-details-service.go
+++ service/car-details-service.go
@@ -1,30 +1,35 @@
 package service
-
-import (
-	"fmt"
-	"net/http"
-)
-
-type CarDetails interface {
-	FetchCarData(brand string)
-	//FetchOwnerData(id string)
-}
-
-type fetchCarDateService struct {}
-
-func NewFetchCarDateService() CarDetails {
-	return &fetchCarDateService{}
-}
-
-const (
-	carDetailsUrl = "https://myfakeapi.com/api/cars/"
-)
-
-func (*fetchCarDateService) FetchCarData(brand string) {
-	pathSegment := "name/"
-	urlApi := carDetailsUrl + pathSegment + brand
-
-	client := http.Client{}
-	resp, _ := client.Get(urlApi)
-	fmt.Println(resp.Body)
-}
+//
+//import (
+//	"fmt"
+//	"net/http"
+//)
+//
+//type CarDetails interface {
+//	FetchCarData(brand string) CarDetails
+//	//FetchOwnerData(id string)
+//}
+//
+//type fetchCarDateService struct{}
+//
+//func NewFetchCarDateService() CarDetails {
+//	return &fetchCarDateService{}
+//}
+//
+//var (
+//	ownerService     = NewOwnerService()
+//	imageService     = NewImageService()
+//	ownerDataChannel = make(chan *http.Response)
+//	imageDataChannel = make(chan *http.Response)
+//)
+//
+//func (*fetchCarDateService) FetchCarData(brand string) CarDetails {
+//
+//
+//	pathSegment := "name/"
+//	urlApi := carDetailsUrl + pathSegment + brand
+//
+//	client := http.Client{}
+//	resp, _ := client.Get(urlApi)
+//	fmt.Println(resp.Body)
+//}
