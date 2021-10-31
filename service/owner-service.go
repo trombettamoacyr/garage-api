6529dbd9faa0c1dc5122b62043fe8ca37dab5ed2
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-10-30 21:14:22 -0300

    creation owner-service

---
+++ service/owner-service.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"encoding/json"
+	"github.com/trombettamoacyr/garage-api/model"
+	"net/http"
+)
+
+type OwnerService interface {
+	FetchDate(id string) model.Owner
+}
+
+type fetchOwnerDataService struct{}
+
+func NewOwnerService() OwnerService {
+	return &fetchOwnerDataService{}
+}
+
+const (
+	ownerApiUrl = "https://myfakeapi.com/api/users/"
+)
+
+func (*fetchOwnerDataService) FetchDate(id string) model.Owner {
+	url := ownerApiUrl + id
+	client := http.Client{}
+	resp, _ := client.Get(url)
+
+	var owner = model.Owner{}
+	json.NewDecoder(resp.Body).Decode(&owner)
+
+	return owner
+}
