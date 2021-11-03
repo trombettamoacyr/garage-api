20951095103a5a151ec8028d6bb4e923b3616a36
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-11-02 23:02:16 -0300

    refact car-service - work in progress

--- service/car-service_test.go
+++ service/car-service_test.go
@@ -1,136 +1,144 @@
 package service
-//
-//import (
-//	"github.com/google/uuid"
-//	"github.com/stretchr/testify/assert"
-//	"github.com/stretchr/testify/mock"
-//	"testing"
-//
-//	"github.com/trombettamoacyr/garage-api/entity"
-//)
-//
-//var (
-//	testServiceNoRepo = NewCarService(nil)
-//)
-//
-//type MockRepository struct {
-//	mock.Mock
-//}
-//
-//func TestValidateSuccess(t *testing.T) {
-//	car := getCarMock()
-//
-//	err := testServiceNoRepo.Validate(&car)
-//
-//	assert.Nil(t, err)
-//}
-//
-//func TestValidateNilCar(t *testing.T) {
-//	errMessageExpected := "The car is null."
-//
-//	err := testServiceNoRepo.Validate(nil)
-//
-//	assert.NotNil(t, err)
-//	assert.Equal(t, errMessageExpected, err.Error())
-//}
-//
-//func TestValidateModelCar(t *testing.T) {
-//	car := getCarMock()
-//	car.Model = ""
-//	errMessageExpected := "The model is empty."
-//
-//	err := testServiceNoRepo.Validate(&car)
-//
-//	assert.NotNil(t, err)
-//	assert.Equal(t, errMessageExpected, err.Error())
-//}
-//
-//func TestValidateBrandCar(t *testing.T) {
-//	car := getCarMock()
-//	car.Brand = ""
-//	errMessageExpected := "The brand is empty."
-//
-//	err := testServiceNoRepo.Validate(&car)
-//
-//	assert.NotNil(t, err)
-//	assert.Equal(t, errMessageExpected, err.Error())
-//}
-//
-//func TestValidateHpCar(t *testing.T) {
-//	car := getCarMock()
-//	car.Hp = 0
-//	errMessageExpected := "The horse power is empty."
-//
-//	err := testServiceNoRepo.Validate(&car)
-//
-//	assert.NotNil(t, err)
-//	assert.Equal(t, errMessageExpected, err.Error())
-//}
-//
-//func TestValidateLicenseCar(t *testing.T) {
-//	car := getCarMock()
-//	car.License = ""
-//	errMessageExpected := "The license is empty."
-//
-//	err := testServiceNoRepo.Validate(&car)
-//
-//	assert.NotNil(t, err)
-//	assert.Equal(t, errMessageExpected, err.Error())
-//}
-//
-//func TestValidateOwnerId(t *testing.T) {
-//	car := getCarMock()
-//	car.OwnerId = ""
-//	errMessageExpected := "The owner-id is empty."
-//
-//	err := testServiceNoRepo.Validate(&car)
-//
-//	assert.NotNil(t, err)
-//	assert.Equal(t, errMessageExpected, err.Error())
-//}
-//
-//func (mock *MockRepository) FindById(id uuid.UUID) (*entity.Car, error) {
-//	args := mock.Called()
-//	result := args.Get(0)
-//	return result.(*entity.Car), args.Error(1)
-//}
-//
-//func (mock *MockRepository) FindAll() ([]entity.Car, error) {
-//	args := mock.Called()
-//	result := args.Get(0)
-//	return result.([]entity.Car), args.Error(1)
-//}
-//
+
+import (
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/trombettamoacyr/garage-api/model"
+	"testing"
+
+	"github.com/trombettamoacyr/garage-api/entity"
+)
+
+var (
+	testServiceNoRepo = NewCarService(nil, nil, nil)
+)
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func TestValidateSuccess(t *testing.T) {
+	car := getCarMock()
+
+	err := testServiceNoRepo.Validate(&car)
+
+	assert.Nil(t, err)
+}
+
+func TestValidateNilCar(t *testing.T) {
+	errMessageExpected := "The car is null."
+
+	err := testServiceNoRepo.Validate(nil)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, errMessageExpected, err.Error())
+}
+
+func TestValidateModelCar(t *testing.T) {
+	car := getCarMock()
+	car.Model = ""
+	errMessageExpected := "The model is empty."
+
+	err := testServiceNoRepo.Validate(&car)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, errMessageExpected, err.Error())
+}
+
+func TestValidateBrandCar(t *testing.T) {
+	car := getCarMock()
+	car.Brand = ""
+	errMessageExpected := "The brand is empty."
+
+	err := testServiceNoRepo.Validate(&car)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, errMessageExpected, err.Error())
+}
+
+func TestValidateHpCar(t *testing.T) {
+	car := getCarMock()
+	car.Hp = 0
+	errMessageExpected := "The horse power is empty."
+
+	err := testServiceNoRepo.Validate(&car)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, errMessageExpected, err.Error())
+}
+
+func TestValidateLicenseCar(t *testing.T) {
+	car := getCarMock()
+	car.License = ""
+	errMessageExpected := "The license is empty."
+
+	err := testServiceNoRepo.Validate(&car)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, errMessageExpected, err.Error())
+}
+
+func TestValidateOwnerId(t *testing.T) {
+	car := getCarMock()
+	car.OwnerId = ""
+	errMessageExpected := "The owner-id is empty."
+
+	err := testServiceNoRepo.Validate(&car)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, errMessageExpected, err.Error())
+}
+
+func (mock *MockRepository) FindAll() (*[]entity.Car, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*[]entity.Car), args.Error(1)
+}
+
+func (mock *MockRepository) FindById(id uuid.UUID) (*entity.Car, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*entity.Car), args.Error(1)
+}
+
+func (mock *MockRepository) FindDetailById(id string) (*model.CarDetail, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*model.CarDetail), args.Error(1)
+}
+
 //func TestFindAll(t *testing.T) {
 //	car := getCarMock()
 //	mockRepo := new(MockRepository)
 //	mockRepo.On("FindAll").Return([]entity.Car{car}, nil)
 //
-//	carReturned, err := NewCarService(mockRepo).FindAll()
+//	carReturned, err := NewCarService(nil, nil, mockRepo).FindAll()
+//	carReturned2 := *carReturned
 //
-//	assert.NotNil(t, carReturned)
-//	assert.Equal(t, car.Id, carReturned[0].Id)
-//	assert.Equal(t, car.Model, carReturned[0].Model)
-//	assert.Equal(t, car.Brand, carReturned[0].Brand)
-//	assert.Equal(t, car.Hp, carReturned[0].Hp)
-//	assert.Equal(t, car.License, carReturned[0].License)
+//	assert.NotNil(t, *carReturned)
+//	assert.Equal(t, car.Id, carReturned2[0].Id)
+//	assert.Equal(t, car.Model, carReturned2[0].Model)
+//	assert.Equal(t, car.Brand, carReturned2[0].Brand)
+//	assert.Equal(t, car.Hp, carReturned2[0].Hp)
+//	assert.Equal(t, car.License, carReturned2[0].License)
 //	assert.Nil(t, err)
 //
 //	mockRepo.AssertExpectations(t)
 //}
-//
-//func (mock *MockRepository) Save(car *entity.Car) (*entity.Car, error) {
-//	args := mock.Called()
-//	result := args.Get(0)
-//	return result.(*entity.Car), args.Error(1)
-//}
-//
+
+func (mock *MockRepository) Save(car *entity.Car) (*entity.Car, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*entity.Car), args.Error(1)
+}
+
 //func TestCreate(t *testing.T) {
 //	car := getCarMock()
 //	mockRepo := new(MockRepository)
 //	mockRepo.On("Save").Return(&car, nil)
 //
-//	carReturned, err := NewCarService(mockRepo).Create(&car)
+//	carReturned, err := NewCarService(nil, nil, mockRepo).Create(&car)
 //
 //	assert.NotNil(t, carReturned)
 //	assert.Equal(t, car.Id, carReturned.Id)
@@ -142,15 +150,15 @@ package service
 //
 //	mockRepo.AssertExpectations(t)
 //}
-//
-//func getCarMock() entity.Car {
-//	carId, _ := uuid.NewRandom()
-//	return entity.Car{
-//		Id:      carId,
-//		Model:   "Model",
-//		Brand:   "Brand",
-//		Hp:      999,
-//		License: "License",
-//		OwnerId: "id",
-//	}
-//}
+
+func getCarMock() entity.Car {
+	carId, _ := uuid.NewRandom()
+	return entity.Car{
+		Id:      carId,
+		Model:   "Model",
+		Brand:   "Brand",
+		Hp:      999,
+		License: "License",
+		OwnerId: "id",
+	}
+}
