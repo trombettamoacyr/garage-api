d74a2a5fb6e344c6c9188a71b324d8b97117dc81
Moacyr Jr Trombetta <junior.trombetta@gmail.com>
2021-11-04 01:04:04 -0300

    test carService - FindById

--- service/car-service_test.go
+++ service/car-service_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/trombettamoacyr/garage-api/model"
 	"testing"
 
 	"github.com/trombettamoacyr/garage-api/entity"
@@ -19,6 +20,40 @@ type MockRepository struct {
 
 type MockInsuranceService struct {
 	mock.Mock
+}
+
+type MockDetailService struct {
+	mock.Mock
+}
+
+func (mock *MockRepository) FindAll() (*[]entity.Car, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*[]entity.Car), args.Error(1)
+}
+
+func (mock *MockRepository) FindById(id uuid.UUID) (*entity.Car, error) {
+	args := mock.Called(id)
+	result := args.Get(0)
+	return result.(*entity.Car), args.Error(1)
+}
+
+func (mock *MockRepository) Save(car *entity.Car) (*entity.Car, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*entity.Car), args.Error(1)
+}
+
+func (mock *MockInsuranceService) FetchValue() string {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(string)
+}
+
+func (mock *MockDetailService) FetchCarData(ownerId string) *model.CarDetail {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*model.CarDetail)
 }
 
 func TestValidateSuccess(t *testing.T) {
@@ -93,30 +128,6 @@ func TestValidateOwnerId(t *testing.T) {
 	assert.Equal(t, errMessageExpected, err.Error())
 }
 
-func (mock *MockRepository) FindAll() (*[]entity.Car, error) {
-	args := mock.Called()
-	result := args.Get(0)
-	return result.(*[]entity.Car), args.Error(1)
-}
-
-func (mock *MockRepository) FindById(id uuid.UUID) (*entity.Car, error) {
-	args := mock.Called()
-	result := args.Get(0)
-	return result.(*entity.Car), args.Error(1)
-}
-
-func (mock *MockRepository) Save(car *entity.Car) (*entity.Car, error) {
-	args := mock.Called()
-	result := args.Get(0)
-	return result.(*entity.Car), args.Error(1)
-}
-
-func (mock *MockInsuranceService) FetchValue() string {
-	args := mock.Called()
-	result := args.Get(0)
-	return result.(string)
-}
-
 func TestFindAll(t *testing.T) {
 	car := getCarMock()
 	var cars []entity.Car
@@ -165,6 +176,63 @@ func TestCreate(t *testing.T) {
 	mockRepo.AssertExpectations(t)
 }
 
+func TestFindById(t *testing.T) {
+	car := getCarMock()
+	carId := car.Id
+	carIdString := carId.String()
+
+	mockRepo := new(MockRepository)
+	mockRepo.On("FindById", carId).Return(&car, nil)
+
+	carPointer, err := NewCarService(nil, nil, mockRepo).FindById(carIdString)
+	carReturned := *carPointer
+
+	assert.NotNil(t, carReturned)
+	assert.Equal(t, car.Id, carReturned.Id)
+	assert.Equal(t, car.Model, carReturned.Model)
+	assert.Equal(t, car.Brand, carReturned.Brand)
+	assert.Equal(t, car.Hp, carReturned.Hp)
+	assert.Equal(t, car.License, carReturned.License)
+	assert.Equal(t, car.OwnerId, carReturned.OwnerId)
+	assert.Equal(t, car.InsuranceValue, carReturned.InsuranceValue)
+	assert.Nil(t, err)
+
+	mockRepo.AssertExpectations(t)
+}
+
+//func TestFindDetailById(t *testing.T) {
+//	car := getCarMock()
+//	carDetail := getCarDetailsMock()
+//	carId := car.Id.String()
+//
+//	mockRepo := new(MockRepository)
+//	mockRepo.On("FindById", carId).Return(&car, nil)
+//
+//	mockDetail := new(MockDetailService)
+//	mockDetail.On("FetchCarData", car.OwnerId).Return(carDetail)
+//
+//	carPointer, err := NewCarService(mockDetail, nil, mockRepo).FindDetailById(carId)
+//	carReturned := *carPointer
+//
+//	assert.NotNil(t, carReturned)
+//	assert.Equal(t, car.Id, carReturned.Car.Id)
+//	assert.Equal(t, car.Model, carReturned.Car.Model)
+//	assert.Equal(t, car.Brand, carReturned.Car.Brand)
+//	assert.Equal(t, car.Hp, carReturned.Car.Hp)
+//	assert.Equal(t, car.License, carReturned.Car.License)
+//	assert.Equal(t, car.OwnerId, carReturned.Car.OwnerId)
+//	assert.Equal(t, carDetail.Owner.Owner.FirstName, carReturned.Owner.Owner.FirstName)
+//	assert.Equal(t, carDetail.Owner.Owner.LastName, carReturned.Owner.Owner.LastName)
+//	assert.Equal(t, carDetail.Owner.Owner.Email, carReturned.Owner.Owner.Email)
+//	assert.Equal(t, carDetail.Owner.Owner.Phone, carReturned.Owner.Owner.Phone)
+//	assert.Equal(t, carDetail.Image.Url, carReturned.Image.Url)
+//	assert.Equal(t, carDetail.Image.ThumbnailUrl, carReturned.Image.ThumbnailUrl)
+//	assert.Nil(t, err)
+//
+//	mockRepo.AssertExpectations(t)
+//	mockDetail.AssertExpectations(t)
+//}
+
 func getCarMock() entity.Car {
 	carId, _ := uuid.NewRandom()
 	return entity.Car{
@@ -173,6 +241,28 @@ func getCarMock() entity.Car {
 		Brand:   "Brand",
 		Hp:      999,
 		License: "License",
-		OwnerId: "id",
+		OwnerId: "ownerId",
+		InsuranceValue: "R$999",
+	}
+}
+
+func getCarDetailsMock() model.CarDetail {
+	return model.CarDetail{
+		Owner: getOwnerMock(),
+		Image: model.Image{
+			Url:          "www.test.com/image.jpg",
+			ThumbnailUrl: "www.test.com/thumb/image.jpg",
+		},
+	}
+}
+
+func getOwnerMock() model.Owner {
+	return model.Owner{
+		Owner: model.OwnerData{
+			FirstName: "Robert",
+			LastName:  "Martin",
+			Email:     "uncle@bob.com.br",
+			Phone:     "112233",
+		},
 	}
 }
